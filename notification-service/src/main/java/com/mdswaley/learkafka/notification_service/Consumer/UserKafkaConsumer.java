6c4427a811ed142f1356a97a62268890ccb4445c
mdswaley <mdswaley075@gmail.com>
2026-04-28 15:23:22 +0530

    Adding user for realtime data access

--- notification-service/src/main/java/com/mdswaley/learkafka/notification_service/Consumer/UserKafkaConsumer.java
+++ notification-service/src/main/java/com/mdswaley/learkafka/notification_service/Consumer/UserKafkaConsumer.java
@@ -1,6 +1,8 @@
 package com.mdswaley.learkafka.notification_service.Consumer;
 
+import com.mdswaley.learkafka.notification_service.event.UserCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,12 @@ import java.security.Key;
 @Service
 @Slf4j
 public class UserKafkaConsumer {
+
+    @KafkaListener(topics = "user-created-topic")
+    public void handleUserCreatedTopic(UserCreatedEvent userCreatedEvent){
+        log.info("handle user created: {}", userCreatedEvent);
+    }
+
     @KafkaListener(topics = "user-random-topic")
     public void handleTopic1(String message){
         log.info("message received1: {}", message);
